17bd5251207dc544510068b1bbf17fb76d952e8b
Ryan <ryan.opitz@mycc.caspercollege.edu>
2023-11-14 20:27:12 -0700

    Followed Tutorial
    Worked up to: Use the database through the models, part 7.
    https://code.visualstudio.com/docs/python/tutorial-django

--- hello_django/hello/views.py
+++ hello_django/hello/views.py
@@ -2,9 +2,18 @@ from django.http import HttpResponse
 import re
 from django.utils.timezone import datetime
 from django.shortcuts import render
+from django.shortcuts import redirect
+from hello.forms import LogMessageForm
+from hello.forms import LogMessage
 
 def home(request):
-    return HttpResponse("Hello, Django!")
+    return render(request, "hello/home.html")
+
+def about(request):
+    return render(request, "hello/about.html")
+
+def contact(request):
+    return render(request, "hello/contact.html")
 
 def hello_there(request, name):
     print(request.build_absolute_uri())
@@ -16,3 +25,15 @@ def hello_there(request, name):
             'date' : datetime.now()
         }
     )
+
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "hello/log_message.html", {"form" : form})
